ff0e7bedd5ec6c6fa0f653964bf40bacde5a98fe
sd-rahulk <83970198+sd-rahulk@users.noreply.github.com>
2021-05-11 19:34:49 +0530

    Create functions.js

---
+++ functions.js
@@ -0,0 +1,5 @@
+var r=document;var a="myBar";var z=1;var p=10;function l(id)
+{location.replace(id)};var elem=r.getElementById(a);
+if (console.log == "#document") {
+    console.error("r is not defined");
+}
